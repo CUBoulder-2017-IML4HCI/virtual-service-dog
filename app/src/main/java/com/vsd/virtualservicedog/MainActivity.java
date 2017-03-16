e83765fca624755fb1cd7014e63fc720552b4a3e
SerendpityZOEY <zoey.zhang513@hotmail.com>
2017-03-16 17:50:22 -0600

    add help activity

--- app/src/main/java/com/vsd/virtualservicedog/MainActivity.java
+++ app/src/main/java/com/vsd/virtualservicedog/MainActivity.java
@@ -1,13 +1,29 @@
 package com.vsd.virtualservicedog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "This is a test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mybtn = (Button) findViewById(R.id.helpbtn);
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getHelp();
+            }
+        });
+    }
+
+    /** Called when the user taps the Help button */
+    public void getHelp() {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
     }
 }

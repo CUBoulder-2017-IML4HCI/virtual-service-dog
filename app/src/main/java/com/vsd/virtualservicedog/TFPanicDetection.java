c3029dc14b839213d9f304c5421c7564c8753765
SerendpityZOEY <zoey.zhang513@hotmail.com>
2017-04-05 16:49:58 -0600

    construct classifier structure

---
+++ app/src/main/java/com/vsd/virtualservicedog/TFPanicDetection.java
@@ -0,0 +1,24 @@
+package com.vsd.virtualservicedog;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+import java.io.IOException;
+
+public class TFPanicDetection implements Classifier{
+    //TODO: exact values TBD
+    private static final int MAX_RESULTS = 3;
+    private static final float THRESHOLD = 0.1f;
+
+    private TensorFlowInferenceInterface inferenceInterface;
+
+    /**
+     * Initializes a native TensorFlow session for classifying user status.
+     * @throws IOException
+     */
+    public static Classifier create(
+
+    )
+    throws IOException {
+        TFPanicDetection pd = new TFPanicDetection();
+        return pd;
+    }
+}

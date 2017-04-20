a1c47cc261b56a5a5b41cce5c0e14f4ebfcf4707
shadybug <cicada8802@gmail.com>
2017-04-20 12:59:26 -0600

    Added permission support

--- app/src/main/java/com/vsd/virtualservicedog/BreathActivity.java
+++ app/src/main/java/com/vsd/virtualservicedog/BreathActivity.java
@@ -1,9 +1,14 @@
 package com.vsd.virtualservicedog;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class BreathActivity extends AppCompatActivity {
@@ -26,5 +31,4 @@ public class BreathActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HelpQ1Activity.class);
         startActivity(intent);
     }
-
 }
